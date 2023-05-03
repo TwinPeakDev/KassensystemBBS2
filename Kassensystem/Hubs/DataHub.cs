32b23134c65e2328090580cd98f9a5c065e9097c
liv <79169882+nnuuvv@users.noreply.github.com>
2023-05-03 11:47:30 +0000

    implement basic savepayment method using signal r

--- Kassensystem/Hubs/DataHub.cs
+++ Kassensystem/Hubs/DataHub.cs
@@ -1,16 +1,38 @@
 using Microsoft.AspNetCore.SignalR;
+using Kassensystem.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 namespace Kassensystem.Hubs;
 
 public class DataHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
+
+    public DataHub(IDbContextFactory<ApplicationDbContext> contextFactory)
+    {
+        _contextFactory = contextFactory;
+    }
+
+
+    public async Task SendMessage(string user, string message) 
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
 
 
-    //public async Task SavePayment()
+    public async Task SavePayment(Sold sold) 
+    {
+        var context = _contextFactory.CreateDbContext();
+
+        await context.SellEntries.AddAsync(sold);
+        await context.SaveChangesAsync();
+
+        await context.DisposeAsync();
+
+        await Clients.All.SendAsync("PaymentSaved");
+    }
 
     
 }
