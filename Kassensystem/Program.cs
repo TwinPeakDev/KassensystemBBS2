50c8464cd34fb4e7abd21d58ed03ac20c5fa1de5
liv <jonas.streun@outlook.com>
2023-05-03 22:14:20 +0200

    new navbar | css from radzen | add products page | more css shenanigans

--- Kassensystem/Program.cs
+++ Kassensystem/Program.cs
@@ -14,7 +14,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 // Add signal r response compression 
 builder.Services.AddResponseCompression(opts =>
