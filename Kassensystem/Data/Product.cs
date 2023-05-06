6c7cb2e2e0ee92e281154d16b3a375cd47bd76f8
liv <jonas.streun@outlook.com>
2023-05-06 14:46:00 +0200

    added basic sales page | need auth to have the logged in user and their cart

--- Kassensystem/Data/Product.cs
+++ Kassensystem/Data/Product.cs
@@ -6,7 +6,7 @@ public class Product
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set;}
-    public string? Name { get; set; } //TODO:Check for null
+    public string Name { get; set; } 
     public double PriceEuro { get; set; }
     public string? Image { get; set; }
     public List<Sold>? SellEntries { get; set; }
