d041c1ac3877caca7e68112c0ba1f6ba25b47b61
elias <elias.stepanik@web.de>
2023-05-06 16:42:25 +0200

    Added File Upload

---
+++ Kassensystem/Controllers/UploadController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kassensystem.Controllers;
+
+[DisableRequestSizeLimit]
+public class UploadController : Controller
+{
+
+    private readonly IWebHostEnvironment _environment;
+    private readonly ILogger<UploadController> _logger;
+
+    public UploadController(IWebHostEnvironment environment, ILogger<UploadController> logger)
+    {
+        _environment = environment;
+        _logger = logger;
+    }
+
+    [HttpPost("upload/single")]
+    public IActionResult Single(IFormFile file)
+    {
+        try
+        {
+            UploadFile(file);
+            return StatusCode(200);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    public async Task UploadFile(IFormFile file)
+    {
+        if (file != null && file.Length > 0)
+        {
+            var imagePath = @"\Uploads";
+            var uploadPath = _environment.WebRootPath + imagePath;
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+            var fullPath = Path.Combine(uploadPath, file.FileName);
+            using (FileStream fileStream = new FileStream(fullPath, FileMode.Create, FileAccess.Write))
+            {
+                await file.CopyToAsync(fileStream);
+                fileStream.Close();
+            }
+        }
+    }
+}
--- Kassensystem/Data/Product.cs
+++ Kassensystem/Data/Product.cs
@@ -9,6 +9,13 @@ public class Product
     public string? Name { get; set; } //TODO:Check for null
     public double PriceEuro { get; set; }
     public string? Image { get; set; }
+    
+    //TODO: Pfand
+    //TODO: nicht mehr als 5000€
+    //TODO: nur bei feature request kosten.
+    //TODO: mann kann auch support punkte kaufen
+    //TODO: mit marko in der signal gruppe mal drüber render
+
     public List<Sold>? SellEntries { get; set; }
 
     //returns true if all non nullable values have been set
